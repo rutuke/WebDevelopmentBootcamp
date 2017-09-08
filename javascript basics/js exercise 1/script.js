52a0a880198be7550844ff4113eced89939f088b
Ruta Puodziunaite <ruta.rutuke@gmail.com>
2017-09-08 23:28:00 +0100

    Java Script Basics - JS Stalker exercise

---
+++ javascript basics/js exercise 1/script.js
@@ -0,0 +1,8 @@
+var firstName = prompt("What's your first name?");
+var secondName = prompt("What's your second name?");
+var age = prompt("What's your age?");
+
+alert(firstName+" "+ secondName);
+alert(age);
+console.log("Your name is " + firstName+" "+secondName);
+console.log("Your age is " + age);
---
+++ javascript basics/script.js
@@ -0,0 +1,5 @@
+alert("Hello from the JS File!");
+
+var firstName = prompt("What's your first name?");
+var secondName = prompt("What's your second name?");
+alert("Nice to meet you "+firstName+secondName);
