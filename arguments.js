50302735289992739b575f7db67e65aa87fba2d9
Ruta Puodziunaite <ruta.rutuke@gmail.com>
2017-09-11 15:29:00 +0100

    JavaScript Arguments Exercises

---
+++ arguments.js
@@ -0,0 +1,26 @@
+function square(num){
+    console.log(num * num);
+}
+
+square(5);
+
+// argument is (num) and num is a placeholder!
+
+function sayHello(name){
+    console.log("Hello there " + name + "!");
+}
+sayHello("Ruta");
+// says hello to the person who imputs their name.
+
+function area(length, width){
+    console.log(length * width);
+}
+area(9, 2);
+// takes the length and the width of a square and gives you its area.
+
+function greet(person1, person2, person3){
+    console.log("Hi "+ person1);
+    console.log("Hi "+ person2);
+    console.log("Hi "+ person3);
+}
+greet("Harry", "Ron", "Hermione");
