abbcef3740af34c191288e6c3e265ccf5be4928a
Ruta Puodziunaite <ruta.rutuke@gmail.com>
2017-09-11 15:28:02 +0100

    JavaScript Functions Exercises

---
+++ functions.js
@@ -0,0 +1,20 @@
+// function doSomething(){
+    // console.log("Hello World");
+//}
+// Reusable bits of code.
+
+// calling the function: doSomething();
+
+function doSomething(){
+    console.log("Hello World!");
+}
+
+doSomething
+
+function singSong(){
+    console.log("Twinkle,Twinkle, little star");
+    console.log("How I wonder what you are");
+    console.log("Up above the world so high");
+    console.log("Like a diamond in the sky!");
+}
+singSong();
